479b46c6c75610f37b25bf0f50e22d09d88d311e
Aaryann Chandola <73146875+Antiarin@users.noreply.github.com>
2024-04-15 19:59:58 +0530

    Time: 55 ms (87.97%) | Memory: 51.9 MB (91.38%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -0,0 +1,16 @@
+function isValid(s: string): boolean {
+    const stack = []
+    for (const i of s) {
+        if (i === '(' || i === '[' || i === '{') stack.push(i);
+        else {
+            if (!stack.length ||
+                (i === ')' && stack[stack.length-1]!== '(') ||
+                (i === '}' && stack[stack.length-1]!== '{') ||
+                (i === ']' && stack[stack.length-1]!== '[')) {
+                return false;
+            }
+            stack.pop()
+        }
+    }
+    return !stack.length
+};
