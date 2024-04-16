e66d1f40a23c5c12b3fe338b1ab948d5f447de22
Aaryann Chandola <73146875+Antiarin@users.noreply.github.com>
2024-04-17 02:03:31 +0530

    Time: 51 ms (89.59%) | Memory: 52 MB (30.12%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.ts
@@ -0,0 +1,18 @@
+function backtrack(res: string[], cur: string, open: number, close: number, max: number) {
+    if (cur.length == max * 2) {
+        res.push(cur);
+        return
+    }
+    if (open < max) {
+        backtrack(res, cur + '(', open + 1, close, max);
+    }
+    if (close < open) {
+        backtrack(res, cur + ')', open, close + 1, max);
+    }
+
+}
+function generateParenthesis(n: number): string[] {
+    const res: string[] = [];
+    backtrack(res, '', 0, 0, n)
+    return res
+};
