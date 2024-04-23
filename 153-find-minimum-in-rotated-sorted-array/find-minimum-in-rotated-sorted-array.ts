ec4a68852f085d9520194adeb9d5e23e03414c26
Aaryann Chandola <73146875+Antiarin@users.noreply.github.com>
2024-04-23 18:46:24 +0530

    Time: 48 ms (92.32%) | Memory: 51.2 MB (79.55%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,3 +1,14 @@
 function findMin(nums: number[]): number {
-    return Math.min(...nums);    
+    let min=Infinity;  
+    let low=0, high=nums.length-1;
+    while(low<=high){
+        const mid=low + Math.floor((high-low+1)/2);
+        if(nums[mid]>nums[nums.length-1]){
+            low=mid+1;
+        }else{
+            min=Math.min(min,nums[mid]);
+            high=mid-1;
+        }
+    }
+    return min;  
 };
