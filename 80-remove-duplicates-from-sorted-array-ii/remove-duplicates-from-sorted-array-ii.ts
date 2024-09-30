45c4a3971bcba5b3e450bf8072273ef7dae0e1ea
Aaryann Chandola <73146875+Antiarin@users.noreply.github.com>
2024-09-30 17:34:07 +0530

    Time: 55 ms (93.52%) | Memory: 52.2 MB (87.49%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    if (nums.length===0) return 0;
+    let k=0;
+    for (let i=0; i<nums.length;i++){
+        if (k<2|| nums[i]>nums[k-2]){
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+    return k;
+};
